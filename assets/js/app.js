c24f98a9da4757e260a80d5877851fb758db2f82
simantaChow <simantachowdhury@gmail.com>
2023-06-14 16:01:50 +0600

    2nd commit
    2nd commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,9 +4,11 @@ $(function () {
         arrows: false,
         dots: true,
         dotsClass:'banner_slider_dots',
-        
-
-    })
-    
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        infinite: true,
+        fade: true,
+    })  
 });
 
