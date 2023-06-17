a2afce7644af9bbe14e59802b00e7b6107b0262f
simantaChow <67776294+simantaChow@users.noreply.github.com>
2023-06-17 10:02:14 +0600

    2nd banner
    2nd banner

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,9 +6,19 @@ $(function () {
         dotsClass:'banner_slider_dots',
         slidesToScroll: 1,
         autoplay: true,
-        autoplaySpeed: 2000,
+        autoplaySpeed: 3000,
         infinite: true,
         fade: true,
+    });
+
+    $('.slider').slick({
+        prevArrow:'<i class="fa-solid fa-chevron-left slider_arrow"></i>',
+        nextArrow:'<i class="fa-solid fa-chevron-right slider_arrow"></i>',
+        slidesToScroll: 1,
+        autoplay: true,
+        slidesToShow: 4,
     })  
+
+
 });
 
