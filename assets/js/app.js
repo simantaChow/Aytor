77f1272a31ffc069cdeaa4b237f87418225c81ac
simantaChow <67776294+simantaChow@users.noreply.github.com>
2023-07-15 09:04:48 +0600

    image centered in category section
    image centered in category section

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,63 +1,71 @@
-// $(function () {
+$(function () {
 
-//     $('.banner_slider').slick({
-//         arrows: false,
-//         dots: true,
-//         dotsClass:'banner_slider_dots',
-//         slidesToScroll: 1,
-//         autoplay: true,
-//         autoplaySpeed: 3000,
-//         infinite: true,
-//         fade: true,
-//     });
+    $('.banner_slider').slick({
+        arrows: false,
+        dots: true,
+        dotsClass:'banner_slider_dots',
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        infinite: true,
+        fade: true,
+        responsive: [
+            {
+              breakpoint: 576,
+              settings: {
+                dots: false
+              }
+            }
+        ]
+    });
 
-//     $('.slider').slick({
-//         prevArrow:'<i class="fa-solid fa-chevron-left slider_arrow"></i>',
-//         nextArrow:'<i class="fa-solid fa-chevron-right slider_arrow"></i>',
-//         slidesToScroll: 1,
-//         autoplay: true,
-//         slidesToShow: 4,
-//     })
+    $('.slider').slick({
+        prevArrow:'<i class="fa-solid fa-chevron-left slider_arrow"></i>',
+        nextArrow:'<i class="fa-solid fa-chevron-right slider_arrow"></i>',
+        slidesToScroll: 1,
+        autoplay: true,
+        slidesToShow: 4,
+    })
 
-//     $('.offer_card_slide').slick({
-//         slidesToScroll: 1,
-//         autoplay: true,
-//         slidesToShow: 2,
-//         dots:true,
-//         arrows:false,
-//         dotsClass:'offer_card_slider_dots',
-//         infinite: true,
-//         autoplaySpeed:2000,
-//     })
+    $('.offer_card_slide').slick({
+        slidesToScroll: 1,
+        autoplay: true,
+        slidesToShow: 2,
+        dots:true,
+        arrows:false,
+        dotsClass:'offer_card_slider_dots',
+        infinite: true,
+        autoplaySpeed:2000,
+    })
     
 
-//     $('.best_seller_products_slider').slick({
-//         arrows: false,
-//         autoplay: false,
-//         slidesToShow: 4,
-//     });
+    $('.best_seller_products_slider').slick({
+        arrows: false,
+        autoplay: false,
+        slidesToShow: 4,
+    });
 
-//     //countdown
-//     $('.clock').countdown('2023/07/30', function(event) {
-//       });
+    //countdown
+    $('.clock').countdown('2023/07/30', function(event) {
+      });
 
-//       $('.lts_news_slider').slick({
-//         slidesToScroll: 1,
-//         autoplay: true,
-//         slidesToShow: 4,
-//         dots:true,
-//         arrows:false,
-//         dotsClass:'lts_news_card_slider_dots',
-//         infinite: true,
-//         autoplaySpeed:2000,
-//     })
+      $('.lts_news_slider').slick({
+        slidesToScroll: 1,
+        autoplay: true,
+        slidesToShow: 4,
+        dots:true,
+        arrows:false,
+        dotsClass:'lts_news_card_slider_dots',
+        infinite: true,
+        autoplaySpeed:2000,
+    })
 
 
-// });
+});
 
-// new VenoBox({
-//     selector:'.video_btn'
-// });
+new VenoBox({
+    selector:'.video_btn'
+});
 
-// const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-// const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
